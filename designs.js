aba805def760a2f36ee7278698611b4cb09e8246
Haritha Rathinakumar <hrathin@g.clemson.edu>
2018-02-12 13:10:21 -0500

    Generate table upon clicking the submit button

--- designs.js
+++ designs.js
@@ -5,4 +5,22 @@
 
 function makeGrid() {
 
+  const body = document.body;
+
+  const gridHeight = document.getElementById("inputHeight").value;
+  const gridWidth = document.getElementById("inputWidth").value;
+
+  const table = document.getElementById("pixelCanvas");
+  const tableBody = document.createElement("tbody");
+
+  for(let i=0; i<=gridHeight; i++){
+    let row = document.createElement("tr");
+    for(let j=0; j<=gridWidth; j++){
+      let cell = document.createElement("td");
+      row.appendChild(cell);
+    }
+    tableBody.appendChild(row);
+  }
+  table.appendChild(tableBody);
+  body.appendChild(table);
 }
